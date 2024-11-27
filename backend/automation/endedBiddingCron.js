a17f31d659f5fb920f5b847379d85497bf1e806e
Aayush-Bisht1 <aayushbisht205@gmail.com>
2024-11-28 00:43:31 +0530

    .

--- backend/automation/endedBiddingCron.js
+++ backend/automation/endedBiddingCron.js
@@ -1,3 +1,8 @@
+import cron from "node-cron";
+import Bidding from "../models/biddingSchema.js";
+
+
+/*
 PORT = 5000
 CLOUDINARY_CLOUD_NAME = dgkbsk9l3
 CLOUDINARY_API_KEY = 815131922146475
@@ -6,3 +11,10 @@ MONGO_URI = mongodb+srv://aayushbisht205:bisht205@cluster0.wx0so.mongodb.net/tes
 JWT_SECRET_KEY = sbfsfsfshfskfhsfsbfssf
 JWT_EXPIRE_IN = 7d
 COOKIE_EXPIRE = 7
+SMTP_HOST = smtp.gmail.com
+SMTP_SERVICE = gmail
+SMTP_PORT = 465
+SMTP_MAIL = 
+SMTP_PASSWORD = nxir dyua ogjp kkyw
+
+*/
